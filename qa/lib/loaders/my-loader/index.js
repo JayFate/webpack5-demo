28204c21f5ba7114d612474a6534745722a76bd8
JayFate <48240828+JayFate@users.noreply.github.com>
2021-01-22 15:30:04 +0800

    Update index.js

--- qa/lib/loaders/my-loader/index.js
+++ qa/lib/loaders/my-loader/index.js
@@ -1,7 +1,4 @@
-const path = require("path");
-
 module.exports = function entryLoader(_, map, meta) {
-  const prjConfigFile = path.resolve(this.projectRoot, "quickapp.config.json");
-  this.addDependency(prjConfigFile);
+  this.addContextDependency(this.projectRoot);
   this.callback(null, '' , map, meta);
 };
